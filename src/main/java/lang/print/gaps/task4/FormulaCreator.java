fbf326c82f2fd6e695a231e4ba2454cbf7d80cb4
Sardorbek <jf.sardorbek@gmail.com>
2022-11-01 19:10:07 +0500

    My first attempt

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 0;
+        int b = 0;
+
+        int i = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+
+        System.out.println('c' + 'h' + 'a' + 'r');
     }
 }
